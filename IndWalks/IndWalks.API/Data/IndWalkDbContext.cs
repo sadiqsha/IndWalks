0b7b7069025befc8da9a7fbaf140ec7c243ea719
sadiqsha <sadiqsha92@gmail.com>
2024-08-14 16:08:30 +0530

    End of Section2

---
+++ IndWalks/IndWalks.API/Data/IndWalkDbContext.cs
@@ -0,0 +1,23 @@
+﻿using IndWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndWalks.API.Data
+{
+    public class IndWalkDbContext : DbContext
+    {
+        public IndWalkDbContext(DbContextOptions<IndWalkDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+
+
+    }
+}
--- IndWalks/IndWalks.API/Program.cs
+++ IndWalks/IndWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using IndWalks.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Sql connection
+builder.Services.AddDbContext<IndWalkDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("IndWalks"));
+
+});
 
 var app = builder.Build();
 
